5d21ab9daa21d51b49d058513cbc1c0c959d47f4
Peter Sun <petersun21@yahoo.com>
2021-09-07 20:32:22 -0500

    a new version with new relays and tft write

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,35 +4,67 @@
 	In this code we have used "[]" to surround our command codes
 	As a bit of a proof of concept for how to use the XC4411 board
 *********************************************/
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+// For the breakout, you can use any 2 or 3 pins
+// These pins will also work for the 1.8" TFT shield
+#define TFT_CS     10
+#define TFT_RST    8  // you can also connect this to the Arduino reset
+                      // in which case, set this #define pin to 0!
+#define TFT_DC    9
+#define TFT_SCLK 13   // set these to be whatever pins you like!
+#define TFT_MOSI 11   // set these to be whatever pins you like!
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
-int current;
-int vol;
+void setupTFT() {
+  Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+  tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+}
 
-int watts;
-
-int inverter1pin;
-int inverter2pin;
-int chargepin;
-
-
-int invStart1;
-int invStart2;
-int invEnd;
 int chargeStart;
 int chargeEnd;
+int InvStart;
+int InvEnd;
+
+void setupTimes() {
+  chargeStart = 1200;
+  chargeEnd   = 360;
+  InvStart    = 360;
+  invEnd      = 1200;
+}
+
+int invPins[5];
+int invState[5];
+int inverterWatts[4];
+int hfeLowSwitch;  //LOW -- off- HIGH - on
 int minutesAfterMidnight;
-int starttime;
+int LastInternetTime;
 
+int currentPin;
+int voltagePin;
+int startTime;
 
-void turnOff(int pin) {
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW);
+void turnOn(int pinNdx) {
+  pinMode(invPins[pinNdx], OUTPUT);
+  pinMode(hfeLowSwitch, OUTPUT);
+  digitalWrite((hfeLowSwitch, HIGH);
+  digitalWrite（invPins[pinNdx], HIGH);
+  invState[pinNdx] = HIGH;
+  delay(300);
+  digitalWrite(invPins[pinNdx], LOW);
+  digitalWrite((hfeLowSwitch, LOW);
 }
-void turnOn(int pin) {
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, HIGH);
-}
 
+void turnOff(int pinNdx) {
+  pinMode(invPins[pinNdx], OUTPUT);
+  pinMode(hfeLowSwitch, OUTPUT);
+  digitalWrite((hfeLowSwitch, LOW);
+  digitalWrite（invPins[pinNdx], HIGH);
+  invState[pinNdx] = LOW;
+  delay(300);
+  digitalWrite(invPins[pinNdx], LOW);
+}
 
 int setValue(String msg, int index, int value)
 {
@@ -40,47 +72,72 @@ int setValue(String msg, int index, int value)
   int v = msg.substring(index).toInt();
   pinMode(LED_BUILTIN , OUTPUT);
   digitalWrite(LED_BUILTIN , HIGH);
-  delay(1000);
+  delay(500);
   if(v > 0) {
-    delay(3000);
+    delay(1500);
   } else {
     v = value;
   }
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
   return v;
 }
 
+void setupRelays() {
+  hfeLowSwitch = 6;
+  for(int i = 0; i < 5; i++) {
+    invPins[i] = i;
+    invState[i] = LOW;
+    hfeLowSwitch = LOW;
+    if(i < 2) {
+      inverterWatts[i] = 290;
+    } else if(i > 1 && i < 4) {
+      inverterWatts[i] = 380;
+    }
+  } 
+}
+
+setupAnalog() {
+  currentPin = A0;
+  voltagePin = A1;
+}
+
+
+
 void setup()
 {
-
-  current = A0;
-  vol     = A1;
-
-  watts       = 0;
-
-  inverter1pin  = 4;
-  inverter2pin  = 5;
-  chargepin     = 6;
-
-
-  invStart1 = 360;
-  invStart2 = 540;
-  invEnd   = 1200;
-  chargeStart = 1200;
-  chargeEnd   = 360;
-  minutesAfterMidnight = 0;
-  starttime = millis()/1000;
-
+  startTime = millis()/60000;
+  setupRelays();
+  setupAnalog();
+  setupTimes();
+  LastInternetTime = -1;
+  setupTFT();
   Serial.begin(SERIAL_BAUD); //same as ESP baud
   delay(2000); //get all information on serial
-  pinMode(inverter1pin, OUTPUT); 
-  pinMode(inverter2pin, OUTPUT); 
-  pinMode(chargepin, OUTPUT);
-  turnOff(inverter1pin);
-  turnOff(inverter2pin);
-  turnOff(chargepin);
+
 }
+
+int analogRead() {
+  float current = 0;
+  float voltage = 0;
+  float watts   = 0;
+  
+  for(int i = 0; i < 1200; i++) {
+     float c = 5.0 * analogRead(currentPin) / (float) 1024;
+     float v = 5.0 * analogRead(voltagePin) / (float) 1024;
+     watts += c*v;
+     current += c;
+     voltage += v;
+  }
+  watts -= current * voltage / (1200 * 1200);
+  current = current * 5.0/200.0;
+  return current * 248 * watts / abs(watts);
+}
+
+int invStart = 360;
+int invEnd   = 1200;
+int chargeStart = 1200;
+int chargeEnd   = 360;
+watts = 0;
 
 void loop()
 {
@@ -91,12 +148,10 @@ void loop()
       switch (message.charAt(pos + 1)) {
         case '0':
           minutesAfterMidnight =  setValue(message, pos + 3, minutesAfterMidnight);
+          LastInternetTime = millis()/1000/60;
           break;
         case '1':
-          invStart1 =  setValue(message, pos + 3, invStart1);
-          break;
-        case '2':
-          invStart2 = setValue(message, pos + 3,invStart2);
+          invStart =  setValue(message, pos + 3, invStart1);
           break;
         case '3':
          invEnd = setValue(message, pos + 3, invEnd);
@@ -112,8 +167,8 @@ void loop()
       }
     }
   }
-
-  if(millis()/1000 > starttime + 120)  {
+  unsigned long currTime = millis()/60000;
+  if(currTime > starttime + 2)  {
     if(invStart1 < invEnd) {
       if( invStart1 <= minutesAfterMidnight && minutesAfterMidnight < invEnd) {
         turnOn(inverter1pin);
